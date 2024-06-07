4ab9f00fecc7dfcefe099223494c2fc939dde19e
AhmedJwad <Ahmednet380@gmail.com>
2024-06-06 22:53:58 -0800

    Add user to authorization

--- Orders/Orders.frondEnd/Pages/Cart/OrdersIndex.razor.cs
+++ Orders/Orders.frondEnd/Pages/Cart/OrdersIndex.razor.cs
@@ -7,7 +7,7 @@ using Orders.Shared.Entities;
 
 namespace Orders.frondEnd.Pages.Cart
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles ="Admin , User")]
     public partial class OrdersIndex
     {
         [Inject] private IRepository repository { get; set; } = null!;
